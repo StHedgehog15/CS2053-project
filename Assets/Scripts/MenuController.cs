bf2283403f7b0f98957cfc32338cfa257b14bf4c
Noah Clements <noahnoah900@gmail.com>
2022-04-07 12:30:49 -0300

    main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    //public Button startGame;
+    //public Button quitGame;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startGame.onClick.AddListener(StartGame);
+        //quitGame.onClick.AddListener(QuitGame);
+
+        // startGame.onClick.AddListener(() => {
+        //     Debug.Log("Start Game");
+        // });
+
+        // quitGame.onClick.AddListener(() => {
+        //     Debug.Log("Quit Game");
+        // });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene("StartLevel");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+}
