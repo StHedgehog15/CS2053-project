e0af623094c91fcb3a96cb09d58ec3809480ac38
Noah Clements <noahnoah900@gmail.com>
2022-04-08 23:17:23 -0300

    fixed compile error w/ spriteGlow

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-//using SpriteGlow;
+using SpriteGlow;
 
 public class GameController : MonoBehaviour
 {
