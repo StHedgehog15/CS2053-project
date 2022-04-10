d909f42edef968f8623708591647f78cae166c10
Keith LeBlanc <39598499+StHedgehog15@users.noreply.github.com>
2022-04-10 19:02:36 -0300

    Update TargeterController.cs
    ...same for targeter

--- Assets/Scripts/TargeterController.cs
+++ Assets/Scripts/TargeterController.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargeterController : MonoBehaviour {
     private Vector3 velocity;
     public float speed;
-    public AudioSource shootSound;
-    public GameController gameControl;
     private Collider2D co;
     private SpriteRenderer rend;
+    public AudioSource shootSound;
     public GameObject playerShot;
+    public Text objText;
+    private bool canFire;
 
     // Start is called before the first frame update
     void Start() {
         velocity = new Vector3(0f, 0f, 0f);
         rend = GetComponent<SpriteRenderer>();
         shootSound = GetComponent<AudioSource>();
+        canFire = true;
+
     }
 
     // Update is called once per frame
@@ -23,30 +27,42 @@ public class TargeterController : MonoBehaviour {
         velocity = new Vector3(0f, 0f, 0f);
         //set the direction based on  input
        // anim.enabled = true;
-        if (Input.GetKey("left")) {
+        if (Input.GetKey("left") || Input.GetKey(KeyCode.A)) {
             velocity = new Vector3(-1f * speed, 0f, 0f);   
         }
-        if (Input.GetKey("right")) {
+        if (Input.GetKey("right") || Input.GetKey(KeyCode.D)) {
             velocity = new Vector3(1f * speed, 0f, 0f);   
         }
-        if (Input.GetKey("down")) {
+        if (Input.GetKey("down") || Input.GetKey(KeyCode.S)) {
             velocity = new Vector3(0f, -1f * speed, 0f); 
         }
-        if (Input.GetKey("up")) {
+        if (Input.GetKey("up") || Input.GetKey(KeyCode.W)) {
             velocity = new Vector3(0f, 1f * speed, 0f);
         }
 
         //move the player
         transform.position += velocity * Time.deltaTime;
         //transform.position = transform.position + velocity * Time.deltaTime * speed;
-
-        if (Input.GetKey(KeyCode.Space)) {
-            shootSound.Play();
+        if (Input.GetKey(KeyCode.Space) && canFire) {
             Fire();
         }
     }
 
-    public void Fire() {
-        Instantiate(playerShot, new Vector3(transform.position.x, transform.position.y,-1), Quaternion.identity);
+    
+    void Fire() {
+        shootSound.Play();
+        GameObject b = Instantiate(playerShot, new Vector3(transform.position.x, transform.position.y, 0f), Quaternion.identity);
+        b.GetComponent<ShotController>().InitPosition(transform.position);
+        canFire = false;
+        StartCoroutine(PlayerCanFireAgain());
+        objText.text = "Reloading...";
     }
+
+    IEnumerator PlayerCanFireAgain() {
+        //this will pause the execution of this method for 3 seconds without blocking
+        yield return new WaitForSecondsRealtime(3);
+        canFire = true;
+        objText.text = "Press Space to fire";
+    }
+
 }
