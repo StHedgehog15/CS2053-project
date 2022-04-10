189c00d5a85ed86863057ec214229d175ceed9d8
Noah Clements <noahnoah900@gmail.com>
2022-04-10 20:35:25 -0300

    credits scene WIP

--- Assets/Scripts/CutsceneTrigger.cs
+++ Assets/Scripts/CutsceneTrigger.cs
@@ -2,14 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
  
 public class CutsceneTrigger : MonoBehaviour
 {
     public PlayableDirector timeline;
     public PlayableAsset cutscene;
     public bool hasPlayed = false;
- 
+    public GameObject textBar;
     public Dialogue dialogue;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        // if textBar is disabled
+        if(!textBar.activeSelf) {
+            SceneManager.LoadScene("Credits");
+        }
+    }
  
     void OnTriggerEnter2D(Collider2D c)
     {
